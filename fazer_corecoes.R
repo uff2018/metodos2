73d9a1486bee3521cac1bcb8befd24a35db1f98b
Gabriel Mizuno <40043614+GMizuno@users.noreply.github.com>
2018-06-28 15:11:52 +0000

    Update fazer_corecoes.R

--- fazer_corecoes.R
+++ fazer_corecoes.R
@@ -1,8 +1,8 @@
 # Melhore a funcao que calcula o intervalo de confianca de uma amostra normal com variancia conhecida ( bilateral)
 
 IC.media = function(x, conf, sigma){
-  n = length(x)
-  x.barra = mean(x)
+  n = length(na.omit(x))
+  x.barra = mean(x,na.rm=TRUE)
   alfa = 1 - conf
   z = qnorm(conf + alfa/2)
   LI = x.barra - z*sigma/sqrt(n)
